6fce5f57dc5169326183a766fab1150f75c44cbb
LarryMburu <168400774+LarryMburu@users.noreply.github.com>
2025-06-20 12:01:16 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,15 +1,15 @@
-def selection_sort(lst):
-    n = len(lst)  
+def selection_sort(unsorted_list):
+    n = len(unsorted_list)  
 
     for i in range(n):
         min_index = i
 
         for j in range(i + 1, n):
-            if lst[j] < lst[min_index]:
+            if unsorted_list[j] < unsorted_list[min_index]:
                 min_index = j 
         if min_index != i:
-            temp = lst[i]
-            lst[i] = lst[min_index]
-            lst[min_index] = temp
+            temp = unsorted_list[i]
+            unsorted_list[i] = unsorted_list[min_index]
+            unsorted_list[min_index] = temp
 
-    return lst  
+    return unsorted_list  
