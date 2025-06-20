03810af30346065288283ee233afb97b8cb67d1d
LarryMburu <168400774+LarryMburu@users.noreply.github.com>
2025-06-20 12:17:14 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,11 +1,7 @@
 def bubble_sort(unsorted_list):
     n = len(unsorted_list)
-
     for i in range(n):
         for j in range(0, n - i - 1):
             if unsorted_list[j] > unsorted_list[j + 1]:
-                temp = unsorted_list[j]
-                unsorted_list[j] = unsorted_list[j + 1]
-                unsorted_list[j + 1] = temp
-
-    return unsorted_list
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+    print("Bubble Sort Result:", unsorted_list)
