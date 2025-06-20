92fdfba7e4696bf93d366eb9c2bc1525fe09398c
LarryMburu <168400774+LarryMburu@users.noreply.github.com>
2025-06-20 11:53:05 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,16 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        if min_index != i:
+            temp = arr[i]
+            arr[i] = arr[min_index]
+            arr[min_index] = temp
+
+    return arr
